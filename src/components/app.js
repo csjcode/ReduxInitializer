cf7b4979414d6a21d746ddb04c22c00cd464c9b6
csjcode <christophersstjohn@gmail.com>
2016-08-08 20:34:18 +0700

    Header component

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Header from './header';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div><Header /></div>
+    );
+  }
+}
