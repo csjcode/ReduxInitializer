4ab00da23a7704a4d6e936142c2848ee8ad10f23
csjcode <christophersstjohn@gmail.com>
2016-08-09 18:17:17 +0700

    Console log to test if requireAuth is working

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 export default function (ComposedComponent) {
   class Authentication extends Component {
     render() {
+      console.log('Rendering', ComposedComponent)
       return <ComposedComponent {...this.props} />
     }
   }
